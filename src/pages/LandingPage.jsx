846b9f722bcb51f6952e35f30c018cccb69b0283
Nutsella <maitegutierrezmdg@gmail.com>
2026-04-13 15:06:55 +0200

    Fix: replace Link for useNavigate on LandingPage

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import './LandingPage.css';
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const LandingPage = () => {
+    const navigate = useNavigate(); // Initialize the navigate function
+    const handleGetStarted = () => {
+    navigate('/products'); // Tell React to move to the products route
+  };
   return (
     <div className="landing-page">
         <div className='landing-page-content'>
             <div className='landing'>
       <h1 className='landing-page-title'>Paradise Nursery</h1>
       <p>Where Greenery Meets Your Home</p>
-      <Link to="/products">
-      <button className='cta-btn'>Get Started</button></Link>
+      <button className='cta-btn' onClick={handleGetStarted}>
+            Get Started
+          </button>
     </div>
     </div>
     </div>
