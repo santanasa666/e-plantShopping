8a7032e763285bd6aa8885af7254753fe460f49b
Nutsella <maitegutierrezmdg@gmail.com>
2026-04-13 15:12:04 +0200

    fix: added import to access about us

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './LandingPage.css';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
+import AboutUs from '../components/AboutUs';
 
 const LandingPage = () => {
     const navigate = useNavigate(); // Initialize the navigate function
